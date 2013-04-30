f8e7427ecd23f2005afb38eececc23b4b3cfa5ef
Fabian Gaertner <fabian.gaertner@hs-furtwangen.de>
2013-05-01 00:53:01 +0200

    [fuProjectGen] 45% done.
    - asking/checking for valid name
    - creating new GUID
    - open, modify, safe Engine.sln

--- fuProjectGen/Program.cs
+++ fuProjectGen/Program.cs
@@ -1,14 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace fuProjectGen
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static bool ValidChars(string name)
         {
+            if (name.Length == 0 || name.Length > 1023) return false;
+
+            var validationRegex = new Regex(@"^(([_]+[a-zA-Z0-9]+\w*)|([a-zA-Z]+\w*))");
+            return validationRegex.IsMatch(name);
+        }
+
+        private static string GetProjectName()
+        {
+            var validName = false;
+            var projectName = "";
+
+            while (!validName)
+            {
+                Console.WriteLine("Please enter a (valid) project name:");
+                Console.Write("> ");
+
+                projectName = Console.ReadLine();
+                validName = ValidChars(projectName);
+
+                Console.WriteLine();
+            }
+
+            return projectName;
+        }
+
+        private static void Error(string msg)
+        {
+            Console.WriteLine(">> ERROR: " + msg);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press enter to exit.");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            Environment.Exit(1);
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("#############################################");
+            Console.WriteLine("##                                         ##");
+            Console.WriteLine("##        > FUSEE - fuProjectGen <         ##");
+            Console.WriteLine("##                                         ##");
+            Console.WriteLine("#############################################");
+            Console.WriteLine("##                                         ##");
+            Console.WriteLine("## WARNING: Always backup your Engine.sln! ##");
+            Console.WriteLine("##                                         ##");
+            Console.WriteLine("#############################################");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // pre-checks
+            if (!File.Exists(@"../Engine.sln"))
+                Error("Couldn't find ../Engine.sln!");
+
+            // get a valid project name
+            var validName = (args.Length == 0) || (!ValidChars(args[0]));
+            var projectName = (validName) ? GetProjectName() : args[0];
+
+            if (!ValidChars(projectName))
+                Error("No valid project name!");
+
+            // open Engine.sln and parse the file
+            var engineSolution = File.ReadAllLines(@"../Engine.sln").ToList();
+            var globalLine = engineSolution.IndexOf("Global");
+            
+            // create GUID for new project
+            var guid = Guid.NewGuid();
+            
+            while (engineSolution.IndexOf(guid.ToString("B")) != -1)
+                guid = Guid.NewGuid();
+            
+            // add project to Engine.sln
+            var slnFile = new SolutionFile(guid, projectName);
+            string slnContent = slnFile.TransformText();
+            
+            engineSolution.Insert(globalLine, slnContent);
+            File.WriteAllLines(@"../Engine.sln", engineSolution);
+
+            // done
+            Console.WriteLine(">> DONE: Sucessfully created new project.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press enter to exit.");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            Environment.Exit(1);
         }
     }
 }
+
---
+++ fuProjectGen/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fuProjectGen
+{
+    partial class SolutionFile
+    {
+        private readonly string _guid;
+        private readonly string _name;
+        
+        public SolutionFile(Guid guid, string name)
+        {
+            _guid = guid.ToString("B");
+            _name = name;
+        }
+    }
+}
